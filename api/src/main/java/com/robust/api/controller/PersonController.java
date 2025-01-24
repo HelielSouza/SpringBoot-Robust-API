b15cb5abb8777f815a61d220752ceb5aa8fe3aac
HelielSouza <helielsouza01234@gmail.com>
2025-01-23 22:42:20 -0300

    Content Negotiation implemented to receive and return JSON, XML and YAML

--- api/src/main/java/com/robust/api/controller/PersonController.java
+++ api/src/main/java/com/robust/api/controller/PersonController.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,10 +16,11 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.robust.api.data.dto.PersonDto;
 import com.robust.api.data.dto.PersonInsertDto;
 import com.robust.api.service.PersonService;
+import com.robust.api.utils.MediaType;
 
 
 @RestController
-@RequestMapping(value = "/person")
+@RequestMapping(value = "/api/v1/person")
 public class PersonController {
 
 	private final String ID = "id";
@@ -28,20 +28,29 @@ public class PersonController {
 	@Autowired
 	private PersonService service;
 	
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces = { MediaType.APPLICATION_JSON,
+							 MediaType.APPLICATION_XML,
+							 MediaType.APPLICATION_YML })
 	public ResponseEntity<List<PersonDto>> findAll() {
 		List<PersonDto> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
-	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/{id}", 
+			    produces = { MediaType.APPLICATION_JSON, 
+			    			 MediaType.APPLICATION_XML,
+			    			 MediaType.APPLICATION_YML })
 	public ResponseEntity<PersonDto> findById(@PathVariable(ID) Long id) {
 		PersonDto obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE},
-				 produces = {MediaType.APPLICATION_JSON_VALUE})
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON, 
+							  MediaType.APPLICATION_XML,
+							  MediaType.APPLICATION_YML },
+				 produces = { MediaType.APPLICATION_JSON, 
+						 	  MediaType.APPLICATION_XML,
+						 	  MediaType.APPLICATION_YML })
 	public ResponseEntity<PersonDto> insert(@RequestBody PersonInsertDto obj) {
 		PersonDto person = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
